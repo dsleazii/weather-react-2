155a999d16ebdce0a5f9089be8fc8159a8eebd6d
dsleazii <141093170+dsleazii@users.noreply.github.com>
2024-01-13 09:53:42 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.css';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+       <div class="container">
       <h1>Weather App</h1>
 
     <footer> This project is coded by{" "}  <a href="https://www.linkedin.com/in/dedra-dearborne-7532b7223/" target="_blank">Dedra D.</a>{" "} and is {" "}
@@ -11,7 +13,7 @@ function App() {
       <a href="https://github.com/dsleazii/weather-react-2" target="_blank">
       
       open-source on Github</a></footer>
-    </div>
+    </div></div>
   );
 }
 
